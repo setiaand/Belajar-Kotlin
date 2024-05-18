ba0e2ef0a774aefe23807dc72572846d28f37a76
setiaand <161325083+setiaand@users.noreply.github.com>
2024-05-18 14:44:30 +0700

    feat/DWL-002: Resolving Conflict Git Ignore (#4)

---
+++ src/test/kotlin/TestKedua.kt
@@ -0,0 +1,21 @@
+fun main(){
+    /* *
+     * Penggunaan val (Read-Only Variabel)
+     *
+     * val digunakan untuk mendeklarasikan variabel yang nilainya tidak bisa diubah
+     * setelah dideklarasikan. Variabel ini mirip dengan final di Java.
+     *
+     * @author: Dwi Prasetya
+     * */
+
+    val name = "Dwi" //Variabel yang hanya dapat dibaca bertipe String
+    println(name) //Output: Dwi
+
+    // name = "Duwai" // ini akan menyebabkan error saat kompilasi
+
+    val age = 25 // Variabel yang hanya dapat dibaca bertipe Int
+    println(age) //Output: 25
+
+    // age = 26 // Ini akan menyebabkan error saat kompilasi
+
+}
