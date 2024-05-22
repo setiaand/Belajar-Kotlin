e912299274108d568055d2c6083111892af94bac
Prass <cahnengrat921@gmail.com>
2024-05-22 21:03:26 +0700

    feat/DWL-003: Latihan Menggunakan Var

---
+++ src/main/kotlin/LatihanKetiga.kt
@@ -0,0 +1,46 @@
+/**
+ * Kelas LatihanKetiga merupakan kelas contoh untuk latihan.
+ *
+ * @since 1.0
+ * @author Dwi Prasetya
+ */
+class LatihanKetiga {
+
+    companion object {
+
+        /**
+         * Pesan yang akan dimodifikasi dan dikirim.
+         *
+         * Nilai awalnya adalah "Hallo".
+         *
+         * @since 1.0
+         */
+        var message: String = "Hallo"
+
+        /**
+         * Mengirimkan pesan yang telah dimodifikasi dengan menambahkan "Arfan" ke dalamnya.
+         *
+         * @return Pesan yang telah dimodifikasi.
+         * @since 1.0
+         */
+        fun sendMessage(): String {
+            message += "Arfan"
+            return message.also { value ->
+                println(value)
+            }
+        }
+    }
+}
+
+/**
+ * Fungsi utama yang menjalankan program.
+ *
+ * Mengubah nilai pesan menjadi "Hi " dan kemudian memanggil fungsi sendMessage untuk
+ * menampilkan dan mengembalikan pesan yang telah dimodifikasi.
+ *
+ * @since 1.0
+ */
+fun main() {
+    LatihanKetiga.message = "Hi "
+    LatihanKetiga.sendMessage()
+}
