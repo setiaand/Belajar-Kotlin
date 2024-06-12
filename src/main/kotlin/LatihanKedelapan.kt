ae75754c036b22732763a11ccb11f105720963b4
Prass <cahnengrat921@gmail.com>
2024-06-12 20:51:50 +0700

    feat/DWT-8: Memahami Penggunaan Boolean

---
+++ src/main/kotlin/LatihanKedelapan.kt
@@ -0,0 +1,42 @@
+/**
+ * Object yang berisi fungsi untuk menampilkan dan mengembalikan nilai boolean.
+ */
+object LatihanKedelapan {
+
+    /**
+     * Menampilkan nilai boolean yang benar (true) dan mengembalikan nilai tersebut.
+     *
+     * @param trueValue Nilai boolean yang akan ditampilkan dan dikembalikan.
+     * @return Nilai boolean yang sama dengan parameter yang diterima.
+     *
+     * Contoh penggunaan:
+     *
+     * val result = LatihanKedelapan.displayTrueValue(true)
+     * println(result) // Output: Nilai Boolean Benar: true
+     *
+     */
+    fun displayTrueValue(trueValue: Boolean): Boolean {
+        return trueValue.also { value ->
+            println("Nilai Boolean Benar: $value")
+        }
+    }
+
+    /**
+     * Menampilkan nilai boolean yang salah (false) dan mengembalikan nilai tersebut.
+     *
+     * @param falseValue Nilai boolean yang akan ditampilkan dan dikembalikan.
+     * @return Nilai boolean yang sama dengan parameter yang diterima.
+     *
+     * Contoh penggunaan:
+     *
+     * val resultFalse = LatihanKedelapan.displayFalseValue(false)
+     * println(resultFalse) // Output: Nilai Boolean Salah: false
+     *
+     */
+    fun displayFalseValue(falseValue: Boolean): Boolean {
+        return falseValue.also { value ->
+            println("Nilai Boolean Salah: $value")
+        }
+    }
+
+}
