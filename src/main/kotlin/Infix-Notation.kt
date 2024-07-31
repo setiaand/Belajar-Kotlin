031563f4d8a8e6a63668c48bcc792ca67ebdcd94
setiaand <161325083+setiaand@users.noreply.github.com>
2024-07-31 20:43:25 +0700

    [DWL] feat: Infix-Notation (#42)
    * feat/DWL: Infix-Notation
    
    * feat/DWL: Infix-Notation

---
+++ src/main/kotlin/Infix-Notation.kt
@@ -0,0 +1,43 @@
+/**
+ * Class untuk menunjukkan penggunaan infix notation.
+ */
+class InfixNotationCalculator {
+
+    /**
+     * Fungsi infix untuk menambahkan dua bilangan.
+     * @param other Bilangan yang akan ditambahkan.
+     * @return Hasil penjumlahan dari this dan other.
+     */
+    infix fun add(other: Int): Int {
+        return this.value + other
+    }
+
+    /**
+     * Fungsi infix untuk mengalikan dua bilangan.
+     * @param other Bilangan yang akan dikalikan.
+     * @return Hasil perkalian dari this dan other.
+     */
+    infix fun multiply(other: Int): Int {
+        return this.value * other
+    }
+
+    infix fun minus(other: Int): Int {
+        return this.value - other
+    }
+
+    var value: Int = 0
+}
+
+fun main() {
+    val calculator = InfixNotationCalculator()
+    calculator.value = 5
+
+    // Menggunakan infix notation untuk menambahkan dan mengalikan
+    val sum = calculator add 3
+    val product = calculator multiply 2
+    val minus = calculator minus 3
+
+    println("Sum: $sum") // Output: Sum: 8
+    println("Product: $product") // Output: Product: 10
+    println("Minus: $minus") //Output: Minus: 2
+}
